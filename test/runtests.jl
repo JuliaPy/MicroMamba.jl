46f685f6ec952ba79628196a72634283ee114797
Christopher Doris <github.com/cjdoris>
2022-11-17 13:26:38 +0000

    more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,11 +3,13 @@ using TestItemRunner
 @run_package_tests
 
 @testitem "MicroMamba" begin
-    @test MicroMamba.available()
-    @test isfile(MicroMamba.executable())
     MicroMamba.STATE.version = VersionNumber(0)
     MicroMamba.STATE.executable = ""
+    @test MicroMamba.available()
+    @test MicroMamba.executable() != ""
+    @test isfile(MicroMamba.executable())
     @test MicroMamba.version() isa VersionNumber
+    @test MicroMamba.version() > VersionNumber(0)
     help = read(MicroMamba.cmd(`--help`), String)
     @test occursin("--help", help)
 end
